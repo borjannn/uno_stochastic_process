91142f53fb967620f24d9b55e663c0d55ffe8a79
borjannn <borjan.dimeski@finki.ukim.mk>
2025-08-27 11:29:41 +0200

    cleaned up code

--- uno_simulation/main.py
+++ uno_simulation/main.py
@@ -1,6 +1,7 @@
 import time
 from statistics import *
 from random import shuffle, randint
+import heapq
 from concurrent.futures import ProcessPoolExecutor
 from functools import partial
 
@@ -18,9 +19,17 @@ class Player:
     def __init__(self, starting_hand):
         self.hand = starting_hand
 
-    def draw_cards(self, deck, n):
-        self.hand += deck[:min(n, len(deck))]
-        return deck[n:]
+    def draw_cards(self, deck, thrown_deck, n):
+        if n >= len(deck):
+            n = n - len(deck)
+            self.hand += deck
+            deck = thrown_deck[:]
+            thrown_deck = []
+            deck.shuffle()
+            self.hand += deck[:n]
+        else:
+            self.hand += deck[:n]
+        return deck[n:], thrown_deck
 
     def __str__(self):
         return f'{self.hand}'
@@ -28,6 +37,7 @@ class Player:
 
 class Game:
     def __init__(self, num_players, num_cards, num_colours, num_starting_cards, reverse, skip, draw2, draw4):
+        self.turn_of_player = 0
         self.reverse = reverse
         self.skip = skip
         self.draw2 = draw2
@@ -40,7 +50,7 @@ class Game:
         self.thrown_deck = []
         for num in range(num_cards):
             for suit in range(num_colours):
-                self.deck.append(Card(num, suit))
+                self.deck.append(Card(num + 1, suit + 1))
         shuffle(self.deck)
         self.players = []
         self.num_players = num_players
@@ -51,6 +61,21 @@ class Game:
             self.deck = self.deck[self.num_starting_cards:]
             self.players.append(Player(starting_hand))
         self.last_card = self.deck.pop(0)
+
+    def best_card(self, hand):
+
+        if self.last_card.num in (7, 14):
+            pass
+
+    def card_action(self, card):
+        if card.num == 7 and self.draw2:
+            self.next_draw = 2
+        elif card.num == 14 and self.draw4:
+            self.next_draw = 4
+        elif card.num == 10 and self.reverse:
+            self.order *= -1
+        elif (card.num == 1 or card.num == 8) and self.skip:
+            self.turn_of_player += self.order
 
     def partial_shuffle(self, quality='high'):
         if quality == 'high':
@@ -66,61 +91,35 @@ class Game:
         elif quality == 'none':
             pass
 
-    def next_person_draw_n(self, player):
-        for k in range(self.next_draw):
-            self.deck = player.draw_cards(self.deck, 1)
-            if len(self.deck) == 0:
-                self.deck = self.thrown_deck[:]
-                shuffle(self.deck)
-                self.thrown_deck = []
+    def try_throw_card(self, card, player):
+        if card.num == self.last_card.num or card.suit == self.last_card.suit:
+            player.hand.remove(card)
+            self.thrown_deck.append(self.last_card)
+            self.last_card = card
+            return True
+        return False
 
     def play(self):
-        i = 0
-
         while True:
             self.turns += 1
-            player = self.players[i]
-            # print("player", i, "turn")
-            # print("top card:", self.last_card, "player hand:", [str(card) for card in player.hand])
+            player = self.players[self.turn_of_player]
 
             if self.next_draw != 0:
-                self.next_person_draw_n(player)
+                self.deck, self.thrown_deck = player.draw_cards(self.deck, self.thrown_deck, self.next_draw)
                 self.next_draw = 0
 
             flag = False
             for card in player.hand:
-                if card.num == self.last_card.num or card.suit == self.last_card.suit:
-                    player.hand.remove(card)
-                    self.thrown_deck.append(self.last_card)
-                    self.last_card = card
-                    if card.num == 6 and self.draw2:
-                        self.next_draw = 2
-                    elif card.num == 13 and self.draw4:
-                        self.next_draw = 4
-                    elif card.num == 10 and self.reverse:
-                        self.order *= -1
-                    elif (card.num == 0 or card.num == 7) and self.skip:
-                        i += self.order
+                if self.try_throw_card(card, player):
                     flag = True
                     break
 
-            if flag is False:
-                self.deck = player.draw_cards(self.deck, 1)
-                if len(self.deck) == 0:
-                    if len(self.thrown_deck) == 0:
-                        self.turns = -1
-                        return
-                    self.deck = self.thrown_deck[:]
-                    shuffle(self.deck)
-                    self.thrown_deck = []
+            if not flag:
+                self.deck, self.thrown_deck = player.draw_cards(self.deck, self.thrown_deck, 1)
 
             if len(player.hand) == 0:
-                # print("Player", i, "won the game!")
-                # print(self.turns)
                 break
-            i += self.order
-            i %= self.num_players
-            # time.sleep(0.1)
+            self.turn_of_player = (self.turn_of_player + self.order) % self.num_players
 
 
 # 1-92    2-50    3-43.5    4-44.25    5-47.5    6-51.35
@@ -131,7 +130,8 @@ primeroci2 = {}
 for np in range(2, 8):
     primerok = []
     for j in range(50000):
-        game = Game(num_players=np, num_cards=14, num_colours=4, num_starting_cards=6, reverse=True, skip=True, draw2=True, draw4=True)
+        game = Game(num_players=np, num_cards=14, num_colours=4, num_starting_cards=6, reverse=True, skip=True,
+                    draw2=True, draw4=True)
         game.play()
         if game.turns != -1:
             primerok.append(game.turns)
@@ -139,8 +139,9 @@ for np in range(2, 8):
 
 for np in range(2, 8):
     primerok = []
-    for j in range(50000):
-        game = Game(num_players=np, num_cards=14, num_colours=4, num_starting_cards=6, reverse=False, skip=False, draw2=False, draw4=False)
+    for j in range(1000):
+        game = Game(num_players=np, num_cards=14, num_colours=4, num_starting_cards=6, reverse=False, skip=False,
+                    draw2=False, draw4=False)
         game.play()
         if game.turns != -1:
             primerok.append(game.turns)
